99be8fc0807fa16e8350068712f0629e284d52f9
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-12-22 18:08:39 +0530

    Initial commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Ayanesh Sarkar on 22/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ayanesh Sarkar on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
